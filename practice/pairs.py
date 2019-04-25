4eb59c29dbf22b0dd7ea3985ab929a4130c0c419
christopherfrazee <chris@christophers-MacBook-Pro.local>
2019-04-24 19:09:10 -0700

    turned into a code snippet

--- practice/pairs.py
+++ practice/pairs.py
@@ -1,34 +1,14 @@
-#!/bin/python3
 import math
 import os
-import random
-import re
 import sys
 
-def sockMerchant(n, ar):
-    result = 0
-    lastnum = 0
-    for n in sorted(ar):
-        print ('number {}'.format(n))
-        if n == lastnum:
-            result = result + 1
-            print('Lastnum is {} n is {}'.format(lastnum, n))
-            lastnum = 0
-        else:
-            lastnum = n
-            print('Lastnum is {} n is {}'.format(lastnum, n))
-            lastnum = n
-    return(result)
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = sockMerchant(n, ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+ar = ["50", "100", "25", "50", "75", "100", "75" , "100"]
+result = 0
+lastnum = 0
+for n in sorted(ar):
+    if n == lastnum:
+        result = result + 1
+        lastnum = 0
+    else:
+        lastnum = n
+print(result)
