b01e41bd05e30132851ccc4e73b858744076d04f
christopherfrazee <chris@christophers-MacBook-Pro.local>
2019-05-06 12:42:09 -0700

    cleanup readme, new oo code

---
+++ practice/oo_stuff.py
@@ -0,0 +1,20 @@
+
+elevators = ["elevator0", "elevator1", "elevator2"]
+
+elevator0Button = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+elevator1Button = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+elevator2Button = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  
+
+
+elevator0.curFloor()    # last floor reached
+elevator0.direction()   # up/down
+elevator0.destination() # next floor in current direction
+
+elevator1.curFloor()    
+elevator1.direction()
+elevator1.destination()
+
+elevator2.curFloor()
+elevator2.direction()
+elevator2.destination()
+
