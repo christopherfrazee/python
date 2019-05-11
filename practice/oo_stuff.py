32cdb14a222907f72ed1d9266129fe37ba821810
christopherfrazee <chris@christophers-MacBook-Pro.local>
2019-05-11 15:07:35 -0700

    defined object / class

--- practice/oo_stuff.py
+++ practice/oo_stuff.py
@@ -6,12 +6,17 @@ elevator0Button = ["off", "on", "off", "off", "off", "off", "off", "off", "off",
 elevator1Button = ["off", "off", "on", "off", "off", "off", "off", "off", "off", "off"]
 elevator2Button = ["off", "off", "off", "off", "off", "off", "off", "off", "off", "off"]  
 
-class elevator:
-    elevator.name() 
-    elevator.CurFloor()    # last floor reached
-    elevator.Direction()   # up/down/idle
-    elevator.Destination() # next floor in current direction
+class Elevator:
+  def __init__(self, name, CurFloor, Direction, Destination):
+        self.name = name
+        self.CurFloor = 0       # last floor reached
+        self.Direction = idle   # up/down/idle
+        self.Destination = 0    # next floor in current directiona
 
+
+Elevator0 = Elevator("0", 0, "idle", 0)
+Elevator1 = Elevator("0", 0, "idle", 0)
+Elevator2 = Elevator("0", 0, "idle", 0)
 
 indices = [i for i, FloorNumButton in enumerate(Elevator0Button) if FloorNumButton == "on"]
 print(indices)
